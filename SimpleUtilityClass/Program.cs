406dcea47b3b1eb9b21ae2cd71b30db36f4c3b50
Максим <tomich7372@gmail.com>
2018-02-01 15:25:58 +0700

    Second lab in 1 lab

---
+++ SimpleUtilityClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleUtilityClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Static Classes ****\n");
+            TimeUtillClass.PrintDate();
+            TimeUtillClass.PrintTime();
+
+            Console.ReadLine();
+        }
+    }
+}
